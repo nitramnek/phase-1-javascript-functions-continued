000d2190daf225670017da3224c1950046dc6c32
Kenneth <nitramnek@gmail.com>
2023-04-20 16:59:55 +0300

    Dine Assignment

--- index.js
+++ index.js
@@ -1,1 +1,74 @@
-// code your solution here
+//k3n5c0d3
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+};
+
+function mondayWork(begin = "go to the office") {
+    return `This Monday, I will ${begin}.`
+};
+
+function wrapAdjective(adjective = "*") {
+    return function(message) {
+        return`You are ${adjective}${message}${adjective}!`
+    };
+}
+
+
+
+
+
+// Class Work
+
+// function razzle() {
+//   console.log("You've been razzled!");
+// }
+// razzle();
+
+// function razzle(lawyer = "Billy", target = "'em") {
+//     console.log(`${lawyer} razzle-dazzles ${target}!`);
+//   }
+// //  razzle()
+// razzle("Methuselah", "T'challah");
+
+// function saturdayFun(){
+//     console.log("This Saturday, I want to ....!")
+// }
+
+// function() {
+//     console.log("Yet more razzling");
+//   }
+
+//   const button = document.getElementById("button");
+//   button.addEventListener("click", function () {
+//     console.log("Yet more razzling");
+//   });
+
+// const fn = function () {
+//     console.log("Yet more razzling");
+//   };
+// fn;
+
+// (function (baseNumber) {
+//     return baseNumber + 3;
+//   })(2);
+
+
+// function outer(greeting, msg = "It's a fine day to learn") {
+//     // 2
+//     const innerFunction = function (name, lang = "Python") {
+//       // 3
+//       return `${greeting}, ${name}! ${msg} ${lang}`; // 4
+//     };
+//     return innerFunction("student", "JavaScript"); // 5
+    
+//   }
+  
+//   outer("Hello");
+
+// function outer(greeting, msg = "It's a fine day to learn") {
+//     const innerFunction = function (name, lang = "Python") {
+//       return `${greeting}, ${name}! ${msg} ${lang}`;
+//     };
+//     return innerFunction;
+//   }
